d282709e98ba815d5e2bbc63629eb47d358dd221
binapani9556 <125079858+binapani9556@users.noreply.github.com>
2024-03-21 00:56:54 +0530

    second commit

--- CoodingAssignmentProject/src/CodingTest/Main.java
+++ CoodingAssignmentProject/src/CodingTest/Main.java
@@ -18,6 +18,8 @@ public class Main {
         
         System.out.println("heyyyy");
         System.out.println("babaaaaaaaaaaaaaa");
+        System.out.println("o0ooooo");
+        
         
         
         
