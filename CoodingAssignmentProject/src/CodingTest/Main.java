b92832362bd4370a2e55922e18a23c1aa0d0a77f
binapani9556 <125079858+binapani9556@users.noreply.github.com>
2024-03-21 00:52:48 +0530

    mama commit

--- CoodingAssignmentProject/src/CodingTest/Main.java
+++ CoodingAssignmentProject/src/CodingTest/Main.java
@@ -17,6 +17,7 @@ public class Main {
         System.out.println("bugu bina");
         
         System.out.println("heyyyy");
+        System.out.println("babaaaaaaaaaaaaaa");
         
         
         
